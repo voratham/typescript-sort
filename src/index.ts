203c55938a62f5ab784f2497693d4e670b528c5d
voratham <dream07312@gmail.com>
2020-02-01 14:40:15 +0700

    Init project interface concepet

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Sorter } from "./Sorter"
+import { NumbersCollection } from "./NumbersCollection"
+import { CharactersCollection } from "./CharactersCollection"
+import { LinkedList } from "./LinkedList"
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log("result final :: ", numbersCollection.data)
+
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+
+
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList)
+sorter.sort()
+linkedList.print()
+
